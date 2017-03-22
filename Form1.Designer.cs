db83dd11c0ee57f72530d52466ea20881cdfbfd0
cipherswan <leskauskas25@gmail.com>
2017-03-22 19:12:50 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,208 @@
+﻿namespace AES_test
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.dirTextBox = new System.Windows.Forms.TextBox();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.EncryptBtn = new System.Windows.Forms.Button();
+            this.DecryptBtn = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pswLabel = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.md5Label = new System.Windows.Forms.Label();
+            this.pswTextBox = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(222, 28);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dirTextBox
+            // 
+            this.dirTextBox.Location = new System.Drawing.Point(12, 30);
+            this.dirTextBox.Name = "dirTextBox";
+            this.dirTextBox.Size = new System.Drawing.Size(204, 20);
+            this.dirTextBox.TabIndex = 1;
+            // 
+            // EncryptBtn
+            // 
+            this.EncryptBtn.Location = new System.Drawing.Point(222, 73);
+            this.EncryptBtn.Name = "EncryptBtn";
+            this.EncryptBtn.Size = new System.Drawing.Size(75, 23);
+            this.EncryptBtn.TabIndex = 2;
+            this.EncryptBtn.Text = "Encrypt";
+            this.EncryptBtn.UseVisualStyleBackColor = true;
+            this.EncryptBtn.Click += new System.EventHandler(this.EncryptBtn_Click);
+            // 
+            // DecryptBtn
+            // 
+            this.DecryptBtn.Location = new System.Drawing.Point(222, 114);
+            this.DecryptBtn.Name = "DecryptBtn";
+            this.DecryptBtn.Size = new System.Drawing.Size(75, 23);
+            this.DecryptBtn.TabIndex = 3;
+            this.DecryptBtn.Text = "Decrypt";
+            this.DecryptBtn.UseVisualStyleBackColor = true;
+            this.DecryptBtn.Click += new System.EventHandler(this.DecryptBtn_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(333, 28);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(297, 269);
+            this.richTextBox1.TabIndex = 4;
+            this.richTextBox1.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(28, 73);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Password:";
+            // 
+            // pswLabel
+            // 
+            this.pswLabel.AutoSize = true;
+            this.pswLabel.Location = new System.Drawing.Point(90, 73);
+            this.pswLabel.Name = "pswLabel";
+            this.pswLabel.Size = new System.Drawing.Size(19, 13);
+            this.pswLabel.TabIndex = 6;
+            this.pswLabel.Text = "....";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(51, 154);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(33, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "MD5:";
+            // 
+            // md5Label
+            // 
+            this.md5Label.AutoSize = true;
+            this.md5Label.Location = new System.Drawing.Point(90, 154);
+            this.md5Label.Name = "md5Label";
+            this.md5Label.Size = new System.Drawing.Size(19, 13);
+            this.md5Label.TabIndex = 7;
+            this.md5Label.Text = "....";
+            // 
+            // pswTextBox
+            // 
+            this.pswTextBox.Location = new System.Drawing.Point(31, 89);
+            this.pswTextBox.Name = "pswTextBox";
+            this.pswTextBox.Size = new System.Drawing.Size(100, 20);
+            this.pswTextBox.TabIndex = 9;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(222, 207);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "Pause";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(222, 236);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 11;
+            this.button3.Text = "Continue";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(222, 265);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 12;
+            this.button4.Text = "Cancel";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(642, 309);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.pswTextBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.md5Label);
+            this.Controls.Add(this.pswLabel);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.DecryptBtn);
+            this.Controls.Add(this.EncryptBtn);
+            this.Controls.Add(this.dirTextBox);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox dirTextBox;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button EncryptBtn;
+        private System.Windows.Forms.Button DecryptBtn;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label pswLabel;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label md5Label;
+        private System.Windows.Forms.TextBox pswTextBox;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
